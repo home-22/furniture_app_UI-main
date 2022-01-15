60f9928921d75c5b28ae591554c161fd7dfa9e74
home-22 <armin.jasarevic.25@gmail.com>
2022-01-15 03:50:46 +0100

    furniture_app_UI-main

---
+++ lib/components/body.dart
@@ -0,0 +1,2 @@
+import 'package:app_in/size_config.dart';
+import 'package:flutter/material.dart';
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,10 +1,47 @@
+import 'package:app_in/components/body.dart';
+import 'package:app_in/constants.dart';
+import 'package:app_in/size_config.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    SizeConfig().init(context);
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: Body(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+        leading: IconButton(
+          icon: SvgPicture.asset(
+            "assets/icons/menu.svg",
+            height: SizeConfig.defaultSize! * 2,
+          ),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            icon: SvgPicture.asset(
+              "assets/icons/scan.svg",
+              height: SizeConfig.defaultSize! * 2.4,
+            ),
+            onPressed: () {},
+          ),
+          const Center(
+            child: Text(
+              "Scan",
+              style: TextStyle(color: kTextColor, fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(
+            width: SizeConfig.defaultSize,
+          ),
+        ]);
   }
 }
