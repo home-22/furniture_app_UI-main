fda11749f41243f08ac11eb5f7e10e4981915f29
home-22 <armin.jasarevic.25@gmail.com>
2022-01-17 21:47:50 +0100

    furniture_app_UI-main

---
+++ lib/screens/product_detail/components/details.dart
@@ -0,0 +1,45 @@
+import 'package:app_in/screens/product_detail/components/bottom.dart';
+import 'package:app_in/screens/product_detail/components/prod_title_price_stars.dart';
+import 'package:app_in/models/items.dart';
+import 'package:flutter/material.dart';
+
+class AboutProduct extends StatelessWidget {
+  final Model product;
+
+  const AboutProduct({Key? key, required this.product}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      padding: const EdgeInsets.only(left: 15, right: 15, top: 20, bottom: 15),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.grey,
+              blurRadius: 20,
+              spreadRadius: 1,
+            ),
+          ]),
+      child: Column(
+        children: [
+          ProdTitleStars(
+            title: product.name,
+            rate: product.rate,
+          ),
+          const SizedBox(height: 10),
+          Text(
+            product.description,
+            style: const TextStyle(color: Colors.grey),
+          ),
+          const Spacer(),
+          ProdBotton(price: product.price),
+        ],
+      ),
+    );
+  }
+}
